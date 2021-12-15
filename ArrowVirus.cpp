186fe99efe6ba49fcb8ed04d773a38c72560ac7f
Ameenbro123 <81590743+Ameenbro123@users.noreply.github.com>
2021-12-15 13:46:02 -0800

    Update ArrowVirus.cpp

--- ArrowVirus.cpp
+++ ArrowVirus.cpp
@@ -1,5 +1,4 @@
-// im not responsible for any idiots that use this for malicious purposes.
-#include "DigiKeyboard.h"
+/* im not responsible for any idiots that use this for malicious purposes.*/
 #define STRINGIFY(x) #x
 
 const char * dk =
@@ -14,7 +13,12 @@ void setup() {
 
 void loop() {
   dk.sendKeyStroke(0);
-  dk.sendKeyStroke
+  dk.sendKeyStroke(MOD_CONTROL_LEFT, KEY_N);
+  dk.print("U suck lol");
+  dk.delay(2500)
+  dk.sendKeyStroke(MOD_SHIFT_LEFT, MOD_CONTROL_LEFT, KEY_Q);
+  dk.delay(1000);
+  dk.sendKeyStroke(MOD_SHIFT_LEFT, MOD_CONTROL_LEFT, KEY_Q);
   dk.delay(5000);
   for (;;){}  // Stops it from repeating. 
 }
